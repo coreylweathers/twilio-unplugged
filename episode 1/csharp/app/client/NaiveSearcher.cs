c2dfe8d470ff99af80622f6f8e89eb23e9fb7651
Corey Weathers <clw895@msn.com>
2020-05-05 20:24:42 -0400

    Adds comments to the NaiveSearch implementation

--- episode 1/csharp/app/client/NaiveSearcher.cs
+++ episode 1/csharp/app/client/NaiveSearcher.cs
@@ -9,15 +9,23 @@ namespace client
     {
         public string[] Search(string text, string pattern)
         {
+
+            // Create a collection to hold the results
             List<string> results = new List<string>();
             
+            // Split the text into an array
             string[] textWords = text.Split(' ');
+
+            // Split the pattern into an array
             string[] patternWords = pattern.Split(' ');
 
+            // Foreach pattern word
             foreach (string patternWord in patternWords)
             {
+                // Foreach word in the text
                 foreach (string word in textWords)
                 {
+                    // If the text word isn't equal to pattern word add to the results collection
                     if (!string.Equals(word, patternWord, StringComparison.CurrentCulture) && !results.Contains(word))
                     {
                         results.Add(word);
@@ -25,6 +33,7 @@ namespace client
                 }
             }
 
+            // return the results collection as an array
             return results.ToArray();
         }
     }
